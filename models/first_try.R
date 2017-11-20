a5466569d586aa12015ec86c4147ed5ae552d8b3
jentzsch-ms <jentzsch.m.s@gmail.com>
2017-11-20 12:13:25 -0500

    minor testing and organizing

--- models/first_try.R
+++ models/first_try.R
@@ -1,13 +1,12 @@
-train.dat <- read.csv(file = "./data/train.csv")
-test.dat <- read.csv(file = "./data/test.csv")
-
 library(ridge)
 library(ggplot2)
+
+train.dat <- read.csv(file = "./data/train.csv")
+test.dat <- read.csv(file = "./data/test.csv")
 
 m1 <- linearRidge(Y ~ ., data = train.dat[,-1])
 m1.pred <- predict(m1, newdata = test.dat)
 
-rmse <- sqrt(1/length(m1.pred)*sum((test.dat$Y-m1.pred)^2))
 
 out.dat <- data.frame(ID = test.dat$ID, Y = m1.pred)
 write.csv(out.dat, file = "./results/firsttry.csv")
