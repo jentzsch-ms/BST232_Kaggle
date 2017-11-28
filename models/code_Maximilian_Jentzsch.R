a4ffa8ddd48b68e2f981ae27b3fceb1c16ccbffa
jentzsch-ms <jentzsch.m.s@gmail.com>
2017-11-28 18:49:22 -0500

    model code for submission

---
+++ models/code_Maximilian_Jentzsch.R
@@ -0,0 +1,35 @@
+final_model <- 
+  function(data_name = "train.csv") {
+    # Step 1: read-in and attach data (DON't CHANGE)
+    data_232 <- read.csv(data_name)
+    attach(data_232)
+    
+    # Step 2:
+    # define the formula of your final chosen model,
+    # do not modify the variable names for exposures (i.e. X1, X2, X3)
+    
+    # final formula prediction model
+    final_formula_pred <- Y ~ X1 + X2 + X3 + Z1 + Z2 + Z5 + Z6 + Z7 + Z9 + 
+      I(X1^2) + X1*X2 + X2*X3 + X1*Z1 + X1*Z6 + X1*Z7 + X1*Z9 + 
+      X2*Z1 + X2*Z7 + X3*Z1 + X3*Z2
+    
+    # final causal model formula
+    final_formula_causal <- Y ~ X1 + I(X1^2) + X2 + I(X2^2) + X3 + I(X3^2) + Z2 + Z3 + Z4 + Z6 + Z7 + Z8 + Z9 + X1*X2 + X1*X3 + X2*X3
+    
+    # Step 3:
+    # fit formula using linear regression, then return the fitted object
+    
+    # final prediction model
+    lm_submit_pred <- lm(formula = final_formula_pred)
+    #final causal model
+    lm_submit_causal <- lm(formula = final_formula_causal)
+    
+    lm_submit <- list(lm_submit_pred, lm_submit_causal)
+    names(lm_submit) <- c("prediction_model", "causal_model")
+    
+    return(lm_submit)
+  }
+
+# now test if your function returns correct output
+print(final_model("./data/train.csv"))
+
